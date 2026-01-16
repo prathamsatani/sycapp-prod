83567453883da50aac318b582b5c7c7d849ac593
prathamsatani <iamprathamsatani2002@gmail.com>
2026-01-16 01:04:47 -0500

    commit

--- server/routes.ts
+++ server/routes.ts
@@ -13,7 +13,9 @@ import { z } from "zod";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
